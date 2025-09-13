8549bb02006515b724a015906171356c48d4995a
Contino Giuliano <continogiuliano627@gmail.com>
2025-09-13 15:50:22 -0300

    feat: linters configured

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.js", "**/*.mjs", "**/*.cjs"],
+    languageOptions: {
+      globals: { ...globals.node, ...globals.es2021 },
+    },
+    ...pluginJs.configs.recommended,
+  },
+];

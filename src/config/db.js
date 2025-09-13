348f6ba03d095f15d1385a997c39e290fd4c27d6
Contino Giuliano <continogiuliano627@gmail.com>
2025-09-13 17:42:21 -0300

    created database

---
+++ src/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+async function connectDB(uri) {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  }
+}
+export default connectDB;
